700fef7578bcbbbced039d7db99cbc8b88e28b7f
liyuhang <liyuhang@greetec.com>
2023-08-23 16:52:31 +0800

    chore: :rewind: 初始化提交

---
+++ .prettierrc.js
@@ -0,0 +1,28 @@
+/*
+ * @Description: Copyright (c) ydfk. All rights reserved
+ * @Author: ydfk
+ * @Date: 2023-08-21 17:53:22
+ * @LastEditors: ydfk
+ * @LastEditTime: 2023-08-21 17:55:09
+ */
+module.exports = {
+  printWidth: 150,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  vueIndentScriptAndStyle: true,
+  singleQuote: false,
+  quoteProps: "as-needed",
+  bracketSpacing: true,
+  trailingComma: "es5",
+  //jsxBracketSameLine: true,
+  bracketSameLine: true,
+  jsxSingleQuote: false,
+  arrowParens: "always",
+  insertPragma: false,
+  requirePragma: false,
+  proseWrap: "never",
+  htmlWhitespaceSensitivity: "strict",
+  endOfLine: "auto",
+  rangeStart: 0,
+};
